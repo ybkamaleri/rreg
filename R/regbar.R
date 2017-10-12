41eeba546c883edf37bda5858cc0abc98c799c6a
ybkamaleri <ybkamaleri@gmail.com>
2017-10-12 17:51:13 +0200

    include data and change reghist to regbar

--- R/regbar.R
+++ R/regbar.R
@@ -1,12 +1,14 @@
-##' Histogram with comparison to national data
+##' Barplot with comparison to national data
 ##'
-##' Create a histogram for each health institutions or health regions
-##' compared to the national data.
+##' Create a barplot with the posibility to differentiate a specific item compared to
+##' the rest. This is useful in a situation when there is a need to show the total
+##' value as compared to each items in the x-axis. A specific example related to the
+##' Norwegian Health Registries is when the sum of each health institutions or health
+##' regions compared to the national data.
 ##'
 ##' @param data Data set
 ##' @param x x-axis
 ##' @param y y-axis
-##' @param flip Flip plot horizontally
 ##' @param show The category that will have distinct colour f.eg. National mean
 ##' @param cut Where to split to show text inside or outside the bar eg. all under 10
 ##'   will show text outside bar while all above 10 will show text inside bar
@@ -14,23 +16,33 @@
 ##' @param title Title for the plot
 ##' @param ylab Label for y-axis
 ##' @param xlab Label for x-axis
+##' @param flip Flip plot horizontally
 ##'
 ##' @import ggplot2
 ##' @export
 
-reghist <- function(data, x, y, flip = FALSE) {
+regbar <- function(data, x, y, flip = FALSE) {
 
+  ## missing data
   if (missing(data)) {
     stop("'data' must be provided",
          call. = FALSE)
   }
 
+  ## missing x or y
+  if (missing(x) | missing(y)) {
+    stop("Both 'x' and 'y' should be specified",
+         call. = FALSE)
+  }
+
+  ## x-axis
   data$xvar <- data[, deparse(substitute(x))]
+  ## yvar
   data$yvar <- data[, deparse(substitute(y))]
 
 
   ## Theme uten title axis-y
-  theme2 <- theme_bw() +
+  themep <- theme_bw() +
     theme(
       axis.text = element_text(size = 10), #text for y and x axis
       axis.ticks.y = element_blank(),
@@ -48,9 +60,10 @@ reghist <- function(data, x, y, flip = FALSE) {
   ## Colour
   col2 <- c("#6baed6","#0845ff")
 
+  ## plot
   p <- ggplot(data) +
     geom_bar(aes(x = reorder(xvar, yvar), yvar), stat = "identity") +
-    theme2
+    themep
 
   ## Flip plot
   if (flip) {
---
+++ doc/sampledata.R
@@ -0,0 +1,37 @@
+## Health institutions
+health <- c("Tawau HF",
+            "Sandakan HF",
+            "Kota Kinabalu HF",
+            "Semporna HF",
+            "Lahad Datu HF",
+            "Kunak HF",
+            "Kalabakan HF",
+            "Merotai HF",
+            "Apas Balung HF",
+            "Sri Menanti HF",
+            "Keningau HF",
+            "Putatan HF",
+            "Tanjung Aru HF",
+            "Sipadan HF",
+            "Pulau Mabul HF")
+
+id <- 1:15
+v2005 <- sample(10:100, size = 15, replace = TRUE)
+v2006 <- sample(10:100, size = 15, replace = TRUE)
+v2007 <- sample(10:100, size = 15, replace = TRUE)
+v2003 <- sample(10:100, size = 15, replace = TRUE)
+v2004 <- sample(10:100, size = 15, replace = TRUE)
+vcase <- rnorm(15, 60, 10)
+vnorm <- c(rnorm(15, 20, 3))
+vext <- round(c(22, 75, runif(12, 18, 25), 65), digits = 0)
+
+data <- data.frame(health, id, v2003, v2004, v2005, v2006, v2007, vcase, vnorm, vext)
+
+library(data.table)
+setDT(data)
+
+## change col names
+setnames(data, 3:ncol(data), gsub("v", "", names(data)[3:ncol(data)]))
+setnames(data, "health", "center")
+
+saveRDS(data, "/home/yusman/Git-work/rreg/data/hfdata.Rds")
