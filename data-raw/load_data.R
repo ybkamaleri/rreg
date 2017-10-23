90daf7575b935f004f07a4b93b2c730f7259b0e5
yuskam <ybkamaleri@gmail.com>
2017-10-23 12:32:32 +0200

    include ldata.rds file for longitudinal data

--- data-raw/load_data.R
+++ data-raw/load_data.R
@@ -1,6 +1,12 @@
-################
-## load data
-################
+
+###################################################################
+## load data and save under ./data folder as .RData or .rda file ##
+###################################################################
+
 
 hfdata <- readRDS("~/Git-work/rreg/data-raw/hfdata.Rds")
 devtools::use_data(hfdata, pkg = "rreg", overwrite = TRUE)
+
+
+yrdata <- readRDS("~/Git-work/rreg/data-raw/ldata.rds")
+devtools::use_data(yrdata, pkg = "rreg", overwrite = TRUE)
