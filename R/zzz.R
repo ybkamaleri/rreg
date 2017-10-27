b78f4bd6a61f35ce37ab05822fb92eb74a70b521
yuskam <ybkamaleri@gmail.com>
2017-10-27 14:35:47 +0200

    onLoad check and install pkg ggplot2 and directlabels if not found

--- R/zzz.R
+++ R/zzz.R
@@ -5,24 +5,41 @@
 ## require(rreg)
 ## detach("package:rreg")
 
-.onAttach <- function(lib, pkg,...){
-   packageStartupMessage(installrWelcomeMessage())
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage(rregWelcomeMessage())
 }
 
 
-installrWelcomeMessage <- function(){
+rregWelcomeMessage <- function(){
    
-   paste("\n",     
-         "Welcome to rreg version ", utils::packageDescription("rreg")$Version, "\n",
-         "\n",
-         # "Type ?aimPlot to access the overall documentation and\n",
-         "More information is available on the Github:\n",
-         "https://github.com/ybkamaleri/rreg/\n",
-         "\n",               
-         "Contact: <ybkamaleri@gmail.com>\n",
-         "Suggestions and bug-reports can be submitted at: https://github.com/ybkamaleri/rreg/issues\n",
-         "\n",
-         "\tTo suppress this message use:\n",
-         "\tsuppressPackageStartupMessages(library(rreg))\n",
-         sep="")
+  paste("\n",     
+        "Welcome to rreg version ", utils::packageDescription("rreg")$Version, "\n",
+        "\n",
+        # "Type ?aimPlot to access the overall documentation and\n",
+        "More information is available on the Github:\n",
+        "https://github.com/ybkamaleri/rreg/\n",
+        "\n",               
+        "Contact: <ybkamaleri@gmail.com>\n",
+        "Suggestions and bug-reports can be submitted at: https://github.com/ybkamaleri/rreg/issues\n",
+        "\n",
+        "\tTo suppress this message use:\n",
+        "\tsuppressPackageStartupMessages(library(rreg))\n",
+        sep="")
+}
+
+.onLoad <- function(libname, pkgname) {
+
+  ## henter pakker eller installere om ikke finnes
+  inspak <- function(pkg){
+    nypkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(nypkg))
+      install.packages(nypkg, dependencies = TRUE, repos = "http://cran.rstudio.com")
+  }
+
+  pakke <- c("ggplot2", "directlabels")
+  inspak(pakke)
+
+  ## Upload libraries
+  library(ggplot2)
+  library(directlabels)
 }
