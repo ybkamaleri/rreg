effda1717ea290223a46f0a679f3d703298d6aae
Yusman Kamaleri <ybkamaleri@gmail.com>
2018-03-14 17:07:18 +0100

    prob with xvar in regtab that can't inherit from regcom

--- R/regcom.R
+++ R/regcom.R
@@ -2,11 +2,7 @@
 ##'
 ##' Create table with column for dummy reference for plot with table
 ##'
-##' @param data Data frame
-##' @param x x-axis
-##' @param ... Additional arguments
-##'
-##' @import ggplot2
+##' @inheritParams regbar
 ##'
 ##' @export
 
@@ -35,6 +31,54 @@ regtab <- function(data, x, ...) {
 
   ## Combine data and new DF
   data <- base::rbind(data, xdf)
+  data$ref <- as.factor(data$ref)
 
   return(data)
+}
+
+
+##' Barplot with point for comparison
+##'
+##' Create a barplot with point to visualise comparison. It is also possible to
+##' include table to show the value of the plot.
+##'
+##' @inheritParams regbar
+##' @param tab Include table
+##'
+##' @import ggplot2
+##'
+##' @export
+
+
+regcom <- function(data, x, yl, yc, tab = TRUE, ...) {
+
+  ## data frame
+  if(missing(data)) {stop("Data must be provided", call. = FALSE)}
+  ##:ess-bp-start::browser@nil:##
+
+  ## x-axis
+  if(missing(x)) {stop("'x' must be provided", call. = FALSE)}
+
+  ## Prepare table with col reference
+  data <- regtab(data, x)
+
+  ## y-axis for local
+  if(missing(yl)) {stop("'yl' for local value must be provided", call. = FALSE)}
+  data$ylocal <- data[, as.character(substitute(yl))]
+
+  ## y-axis for national
+  if(missing(yc)) {stop("'yc' for comparison value must be provided", call. = FALSE)}
+  data$ycomp <- data[, as.character(substitute(yc))]
+
+
+  p <- ggplot(data, aes(ref, ylocal)) +
+    geom_bar(stat = "identity") +
+    geom_point(aes(ref, ycomp), stat = "identity",
+               shape = 18, size = 8, color = "blue") +
+    coord_flip() +
+    scale_x_discrete(breaks = factor(data$ref), labels = data$xvar)
+
+  return(p)
+
+
 }
