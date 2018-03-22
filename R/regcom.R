1dc7590dbd35a763cc5a44320432dfb9ae8e6f48
ybkamaleri <ybkamaleri@gmail.com>
2018-03-22 13:54:02 +0100

    regcome with examples

--- R/regcom.R
+++ R/regcom.R
@@ -16,6 +16,19 @@
 ##'
 ##' @import ggplot2
 ##'
+##' @examples
+##' library("rreg")
+##' regcom(data = hfdata, x = inst, yl = case2, yc = case1)
+##'
+##' # include table
+##' regcom(data = hfdata, x = inst, yl = case2, yc = case1, tab = FALSE)
+##'
+##' # keep original order
+##' regcom(data = hfdata, x = inst, yl = case2, yc = case1, scale = "Percentage", ascending = FALSE)
+##'
+##' # text for table rotate 10%
+##' regcom(data = hfdata, x = inst, yl = case2, yc = case1, lab1="Tawau", lab2="Negara", rotate=10)
+##'
 ##' @export
 
 regcom <- function(data, x, yl, yc, tab = TRUE,
